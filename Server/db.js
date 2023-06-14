e172972c24495b9de63f96424ded89ac6902699e
Hirumalshi95 <gunawardhanath.20@uom.lk>
2023-06-14 13:32:31 +0530

    updated Mongo db link

--- Server/db.js
+++ Server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gamagepdw20:SportsArena5@arenacluster.zqtz2vv.mongodb.net/ArenaDB?retryWrites=true&w=majority/Arena', 
+mongoose.connect('mongodb+srv://user:Sports@ixd@cluster0.styvg5e.mongodb.net/ArenaDB?retryWrites=true&w=majority/Arena', 
 {
     useNewUrlParser: true,
     useUnifiedTopology:true
