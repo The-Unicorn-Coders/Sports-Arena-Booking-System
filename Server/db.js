c9e0366b6c330b9643cf08203ab49043171ce1a7
Sandumini123 <121333059+Sandumini123@users.noreply.github.com>
2023-06-22 13:29:33 +0530

    update

--- Server/db.js
+++ Server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gamagepdw20:SportsArena5@arenacluster.zqtz2vv.mongodb.net/ArenaDB?retryWrites=true&w=majority/Arena', 
+mongoose.connect('mongodb+srv://user:123@cluster0.styvg5e.mongodb.net/ArenaDB?retryWrites=true&w=majority/Arena',
 {
     useNewUrlParser: true,
     useUnifiedTopology:true
---
+++ Server/routes/arenaRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const arena = require('../models/arena')
+
+router.get(/get(req,res) => {
+    try {
+    }catch(error){
+
+    }
+});
