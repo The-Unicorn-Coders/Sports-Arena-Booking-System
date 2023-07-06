15ff795a00dd48c94c68f49179231b6dc9e6d233
Madeeha20 <121339972+Madeeha20@users.noreply.github.com>
2023-07-06 17:27:59 +0530

    stripe payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState} from 'react';
 import axios from 'axios';
-import Payment from "./components/payment";
+import Payment from "./Components/payment";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
