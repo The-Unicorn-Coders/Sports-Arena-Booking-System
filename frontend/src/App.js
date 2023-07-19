82e7c67ed3e2b88a0478f7bad20955db13294080
Hirumalshi95 <gunawardhanath.20@uom.lk>
2023-07-19 21:39:12 +0530

    updated path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import HomeScreen from "./Pages/HomeScreen";
 import { Switch } from "@mui/material";
 import AvailabilityMain from "./Pages/LiveAvailabilityPage/AvailabilityMain";
 import Bmain from "./Pages/BookingPage/Bmain";
+import { Explore } from "@mui/icons-material";
+import ExpolerPart from "./Pages/ExpolerPart";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
           <Route path="/" element={<HomeScreen />} />
           <Route path="/booking" element={<AvailabilityMain />} />
           <Route path="/filtering"  element={<Bmain/>} />
+          <Route path="/explorer"  element={<ExpolerPart/>} />
         </Routes>
       </Router>
     </div>
